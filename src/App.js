220d3f3c044471cb39e2ef0ff84ee6b53eec249f
Mart <themcman@gmail.com>
2020-04-05 21:18:59 -0500

    最初

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "./firebase";
+
+firebase
+  .firestore()
+  .collection("times")
+  .add({
+    title: "whatever",
+    seconds: 25
+  });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">ok. cool</div>;
 }
 
 export default App;
